05a9aa2080a6bec9e14497fe8c4715ee53c6853c
Licosity <68554081+Licosity@users.noreply.github.com>
2025-05-21 15:09:49 +0200

    main.py

--- Paris/main.py
+++ Paris/main.py
@@ -1,24 +1,23 @@
-from Paris.street import StreetLampController
+
+import board
 import time
+from street import StreetLampController
 
-# GPIO pin for NeoPixel strip
-NEOPIXEL_PIN = 16
+NEOPIXEL_PIN = board.D18  # GPIO18 (physical pin 12) 
+MOTION_SENSOR_PINS = [17, 27, 22, 5, 6, 13]  
 
-# GPIO pins for motion sensors 
-MOTION_SENSOR_PINS = [10, 11, 12, 13, 14, 15]
-
-# Create lamp system instance
 lamp_system = StreetLampController(
     neopixel_pin=NEOPIXEL_PIN,
     motion_pins=MOTION_SENSOR_PINS,
-    num_leds=6,
     color=(255, 255, 255),  
-    glow_time=2500,
-    neighbor_delay=500,
     use_neighbor_logic=True
 )
 
-# Main loop
-while True:
-    lamp_system.update()
-    time.sleep_ms(50)
+try:
+    while True:
+        lamp_system.update()
+        time.sleep(0.05)
+except KeyboardInterrupt: # ctrl C to stop programm properly
+    print("Cleaning up GPIO...")
+    import RPi.GPIO as GPIO
+    GPIO.cleanup()
