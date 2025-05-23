0a59673f5dc0760bbc69ef65385b15d5753ac880
Licosity <68554081+Licosity@users.noreply.github.com>
2025-05-23 14:00:05 +0200

    Update main.py

--- Paris/main.py
+++ Paris/main.py
@@ -6,38 +6,36 @@ import neopixel
 import RPi.GPIO as GPIO
 
 from street import StreetLampController
-from env_controller import EnvController
-
+from IndoorF import EnvController
+from house import House
 
 #street constants
 GLOW_TIME = 3  # seconds
 DELAY_TIME = 0.3  # seconds
-NUM_LAMPS = 6
 WHITE = (255, 255, 255)
 NEOPIXEL_PIN = board.D18  # pin 12 
-MOTION_SENSOR_PINS = [17, 27, 22, 5, 6, 13]  
+MOTION_PINS = [17, 27, 22, 5, 6, 13]  
+
+
 
 
 def main():
-    controller = EnvController(
-       
-        dht_pin=4,
-        fan_pin=17,
-        led_pin=18,
-        num_leds=8,
-        ldr_channel=0 
-    )
+    house = House()
+    IndoorF = EnvController()
+    street = StreetLampController()
+
+    try:
+        while True:
+            IndoorF.update()
+            street.update()
+            house.update()
+            time.sleep(0.08)
+    except KeyboardInterrupt: # ctrl C to stop programm 
+        print("Cleaning up program...")
+        IndoorF.cleanup()
+        GPIO.cleanup()
 
 
-try:
-    while True:
-        controller.update()
-        lamp_system.update()
-        time.sleep(0.05)
-except KeyboardInterrupt: # ctrl C to stop programm 
-    print("Cleaning up GPIO...")
-    controller.cleanup()
-    GPIO.cleanup()
 
 
 
@@ -45,7 +43,3 @@ if __name__ == "__main__":
     main()
 
     
-#PV:
-# from solar_tracker import SolarTracker
-# tracker = SolarTracker()
-# threading.Thread(target=tracker.run).start()
