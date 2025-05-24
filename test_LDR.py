149b4628e95e43b324452756abc2eabc804722d3
Licosity <68554081+Licosity@users.noreply.github.com>
2025-05-24 17:28:56 +0200

    Update test_LDR.py

--- test_LDR.py
+++ test_LDR.py
@@ -13,13 +13,13 @@ chan1 = AnalogIn(ads, ADS.P1)
 chan2 = AnalogIn(ads, ADS.P2)
 chan3 = AnalogIn(ads, ADS.P3)
 while True:
-print("Kanal 0: ","{:>5}\t{:>5.3f}".format
-(chan0.value, chan0.voltage))
-print("Kanal 1: ","{:>5}\t{:>5.3f}".format
-(chan1.value, chan1.voltage))
-print("Kanal 2: ","{:>5}\t{:>5.3f}".format
-(chan2.value, chan2.voltage))
-print("Kanal 3: ","{:>5}\t{:>5.3f}".format
-(chan3.value, chan3.voltage))
-print("----")
-time.sleep(1)
+  print("Kanal 0: ","{:>5}\t{:>5.3f}".format
+  (chan0.value, chan0.voltage))
+  print("Kanal 1: ","{:>5}\t{:>5.3f}".format
+  (chan1.value, chan1.voltage))
+  print("Kanal 2: ","{:>5}\t{:>5.3f}".format
+  (chan2.value, chan2.voltage))
+  print("Kanal 3: ","{:>5}\t{:>5.3f}".format
+  (chan3.value, chan3.voltage))
+  print("----")
+  time.sleep(1)
